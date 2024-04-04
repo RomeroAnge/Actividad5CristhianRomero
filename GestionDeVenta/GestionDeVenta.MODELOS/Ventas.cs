f721c9427930487ccc90bf4d02423b68f3c20afc
RomeroCristhian <cristianrv426@gmail.com>
2024-04-04 06:00:03 -0400

    Actividad5CristhianRomero

---
+++ GestionDeVenta/GestionDeVenta.MODELOS/Ventas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionDeVenta.MODELOS
+{
+    public class Ventas
+    {
+        public int IdVenta { get; set; }
+        public DateTime FechaVenta{ get; set; }
+        public decimal TotalVenta{ get; set; }
+
+    }
+}
---
+++ GestionDeVenta/GestionDeVenta.VISTA/VentasVistas/RegistrarVentaVista.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace GestionDeVenta.VISTA.VentasVistas
+{
+    partial class RegistrarVentaVista
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dateTimePicker1 = new DateTimePicker();
+            label1 = new Label();
+            dataGridView1 = new DataGridView();
+            button1 = new Button();
+            button2 = new Button();
+            label3 = new Label();
+            label4 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(198, 46);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(289, 27);
+            dateTimePicker1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(63, 46);
+            label1.Name = "label1";
+            label1.Size = new Size(129, 20);
+            label1.TabIndex = 1;
+            label1.Text = "FECHA DE VENTA:";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(27, 79);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(582, 188);
+            dataGridView1.TabIndex = 4;
+
+            dataGridView1.CellValueChanged += dataGridView1_CellValueChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(615, 92);
+            button1.Name = "button1";
+            button1.Size = new Size(40, 29);
+            button1.TabIndex = 5;
+            button1.Text = "➕";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(63, 344);
+            button2.Name = "button2";
+            button2.Size = new Size(113, 29);
+            button2.TabIndex = 6;
+            button2.Text = "GUARDAR";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(63, 294);
+            label3.Name = "label3";
+            label3.Size = new Size(125, 20);
+            label3.TabIndex = 8;
+            label3.Text = "TOTAL DE VENTA:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(194, 294);
+            label4.Name = "label4";
+            label4.Size = new Size(17, 20);
+            label4.TabIndex = 9;
+            label4.Text = "0";
+            // 
+            // RegistrarVentaVista
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(685, 392);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
+            Controls.Add(label1);
+            Controls.Add(dateTimePicker1);
+            Name = "RegistrarVentaVista";
+            Text = "RegistrarVentaVista";
+            Load += RegistrarVentaVista_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DateTimePicker dateTimePicker1;
+        private Label label1;
+        private DataGridView dataGridView1;
+        private Button button1;
+        private Button button2;
+        private Label label3;
+        private Label label4;
+    }
+}
